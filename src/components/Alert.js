1f7f366c45651c7803a0f3251d18eabfba23870a
cebanu maxim <cebanumaxim31@gmail.com>
2022-10-27 12:09:36 +0300

    #office: Alert component. part 2

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
-export const Alert = ({ alert }) => {
+export const Alert = () => {
+    const { alert, hide } = useContext(AlertContext)
 
+    if (!alert) return null
 
     return (
         <div
@@ -21,6 +24,7 @@ export const Alert = ({ alert }) => {
                     'borderWidth': 0,
                     'backgroundColor': 'transparent',
                 } }
+                onClick={ hide }
             >
                 <span aria-hidden="true">&times;</span>
             </button>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,22 @@
-import { React } from "react"
+import { React, useContext } from "react"
+import { AlertContext } from "../context/alert/alertContext"
 
 export const Search = () => {
+    const { show } = useContext(AlertContext)
+
+    const onSubmit = event => {
+        if (event.key === 'Enter') {
+            show('This is aler!')
+        }
+    }
+
     return (
         <div className="form-group mb-4">
             <input
                 type="text"
                 className="form-control"
                 placeholder="username"
+                onKeyPress={ onSubmit }
             />
         </div>
     )
