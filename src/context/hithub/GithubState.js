de74246a640973d20893520beaad5ef71e4c1f38
cebanu maxim <cebanumaxim31@gmail.com>
2022-10-27 14:12:59 +0300

    #office: github state, p2

--- src/context/hithub/GithubState.js
+++ src/context/hithub/GithubState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from "react"
+import { CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING } from "../types"
 import { GithubContext } from "./githubContext"
 import { githubReducer } from "./githubReducer"
 
@@ -12,9 +13,45 @@ export const GithubState = ({ children }) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
+    const search = async vlaue => {
+        setLoading()
+        //...
+
+        dispatch({
+            type: SEARCH_USERS,
+            payload: []
+        })
+    }
+
+    const getUser = async name => {
+        setLoading()
+        //...
+
+        dispatch({
+            type: GET_USER,
+            payload: {}
+        })
+    }
+
+    const getRepos = async name => {
+        setLoading()
+        //...
+
+        dispatch({
+            type: GET_REPOS,
+            payload: []
+        })
+    }
+
+    const clearUsers = () => dispatch({ type: CLEAR_USERS })
+
+    const setLoading = () => dispatch({ type: SET_LOADING })
+
+    const { user, users, repos, loading } = state
+
     return (
         <GithubContext.Provider value={ {
-
+            setLoading, search, getUser, getRepos, clearUsers, user, users, repos, loading
         } }>
             { children }
         </GithubContext.Provider>
